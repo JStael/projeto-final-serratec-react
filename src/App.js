88a51ecd4b40aa6e3368823720796944f9d67dc5
Afonso Nunes Rodrigues <70178659+afonsonrodrigues@users.noreply.github.com>
2021-06-23 13:55:06 -0300

    Produtos

--- src/App.js
+++ src/App.js
@@ -1,49 +1,52 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Cadastro from './pages/Cadastro';
-import Home from './pages/Home';
-import './App.css';
-import Login from '../src/pages/Login'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Cadastro from "./pages/Cadastro";
+import Home from "./pages/Home";
+import "./App.css";
+import Login from "../src/pages/Login";
+import Produtos from "./pages/Produtos";
 
 function App() {
+    const menu = [
+        {
+            titulo: "Inicio",
+            link: "/",
+        },
+        {
+            titulo: "Produtos",
+            link: "/produtos",
+        },
+        {
+            titulo: "Cadastre-se",
+            link: "/cadastro",
+        },
+        {
+            titulo: "Login",
+            link: "/login",
+        },
+    ];
 
-  const menu = [
-    {
-      titulo: 'Inicio',
-      link: '/'
-    },
-    {
-      titulo: 'Produtos',
-      link: '/produtos'
-    },
-    {
-      titulo: 'Cadastre-se',
-      link: '/cadastro'
-    },
-    {
-      titulo: 'Login',
-      link: '/login'
-    },
-  ];
-
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar navbar={menu} />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/cadastro">
-            <Cadastro />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar navbar={menu} />
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/cadastro">
+                        <Cadastro />
+                    </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/produtos">
+                        <Produtos />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
